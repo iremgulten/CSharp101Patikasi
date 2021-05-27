8978730a40db5913dd1e39544a78ed6afe6c559a
İrem Gülten <iremgulten@gmail.com>
2021-05-27 23:51:33 +0300

    If Else ve Ternary If Karar Yapisi

---
+++ Ders8_KararYapilari/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ders8_KararYapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  *** If Else & Ternary If
+            //If Else 
+            int time = DateTime.Now.Hour;
+            
+            if(time>=6 && time<11){
+                Console.WriteLine("Gunaydin");
+            }else if(time<=18){
+                Console.WriteLine("Iyi Gunler");
+            }else{
+                Console.WriteLine("Iyi Geceler");
+            }
+
+            //Ternary If
+            string sonuc = time<=18 ? "Iyi Gunler":"Iyi Geceler";
+
+            sonuc = time>=6 && time<11 ? "Gunaydin" : time<=18 ? "Iyi Gunler" : "Iyi Geceler";
+
+            Console.WriteLine(sonuc);
+            */
+        }
+    }
+}
