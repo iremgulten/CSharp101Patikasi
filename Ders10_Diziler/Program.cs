3d5444df72af59ba38ce5dffac59c50de6b17777
İrem Gülten <iremgulten@gmail.com>
2021-05-28 13:53:51 +0300

    Array sinifi metodlari eklendi

--- Ders10_Diziler/Program.cs
+++ Ders10_Diziler/Program.cs
@@ -6,7 +6,7 @@ namespace Ders10_Diziler
     {
         static void Main(string[] args)
         {
-            //Dizi tanimlama
+/* Dizi tanimlama - Deger Atama - Erisim
             string[] renkler = new string[5];
 
             string[] hayvanlar = {"Kedi","Kopek","Kus","Maymun"};
@@ -40,6 +40,63 @@ namespace Ders10_Diziler
                 toplam+=sayi;
 
             Console.WriteLine("Ortalama :"+toplam/diziUzunlugu);
+*/
+
+// Array Sinifi Metodlari
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+            Console.WriteLine("*** Sirasiz Dizi***");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            // output 23 12 4 86 72 3 11 17
+            
+
+            Console.WriteLine("*** Sirali Dizi***"); 
+            Array.Sort(sayiDizisi);  //kucukten buyuge siralama
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //output 3 4 11 12 17 23 72 86
+
+
+            //Clear
+            Console.WriteLine("*** Array Clear***");
+            Array.Clear(sayiDizisi,2,2);    //2. indisten itibaren 2 eleman siralanir
+            //output 3 4 0 0 17 23 72 86
+
+
+            //Reverse -> Diziyi ters cevirme
+            Console.WriteLine("*** Array Reverse***");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //output 86 72 23 17 0 0 4 3
+
+            //IndexOf -> Verilen eleman dizide varsa indis doner
+            Console.WriteLine("*** Array IndexOf***");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));  
+            //output 2
+
+
+            //Resize -> Yeniden Boyutlandirma
+            Console.WriteLine("*** Array Resize***");
+            Array.Resize(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //output 86 72 23 17 0 0 4 3 99
+
+
+
+
+
 
 
 
