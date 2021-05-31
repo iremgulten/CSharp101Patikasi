80506d580073df86a75a11f57301cacb48254710
İrem Gülten <iremgulten@gmail.com>
2021-05-31 21:56:49 +0300

    Constructor Metot incelenerek ilgili koda eklendi

--- Ders16_SinifKavrami/SinifSozDizimi/Program.cs
+++ Ders16_SinifKavrami/SinifSozDizimi/Program.cs
@@ -21,22 +21,24 @@ namespace SinifSozDizimi
             Protected -> Sadece tanimlandigi sinif ya da miras alan sinif
             */
 
-            Calisan calisan1 = new Calisan();
-            calisan1.Ad = "Ayse";
-            calisan1.Soyad = "Kara";
-            calisan1.No = 23425634;
-            calisan1.Ad = "İnsan Kaynaklari";
-
+            Calisan calisan1 = new Calisan("Ayse","Kara", 23425634, "İnsan Kaynaklari");
+            //calisan1.Ad = "Ayse";
+            //calisan1.Soyad = "Kara";
+            //calisan1.No = 23425634;
+            //calisan1.Ad = "İnsan Kaynaklari";
             calisan1.CalisanBilgileri();
 
             Console.WriteLine("****************");
-            Calisan calisan2 = new Calisan();
-            calisan2.Ad = "Deniz";
-            calisan2.Soyad = "Arda";
-            calisan2.No = 24625634;
-            calisan2.Ad = "Satin Alma";
-
+            Calisan calisan2 = new Calisan("Deniz", "Arda", 24625634, "Satin Alma");
+            //calisan2.Ad = "Deniz";
+            //calisan2.Soyad = "Arda";
+            //calisan2.No = 24625634;
+            //calisan2.Ad = "Satin Alma";
             calisan2.CalisanBilgileri();
+
+            Calisan calisan3 = new Calisan("Aydin", "Sancak");
+            calisan3.CalisanBilgileri();
+            //Yazdirma esnasinda gonderilmeyen degerler icin tiplerin default degerleri verilir
 
 
 
@@ -47,6 +49,19 @@ namespace SinifSozDizimi
         public string Soyad;
         public int No;
         public string Departman;
+        public Calisan(string ad, string soyad, int no, string departman){
+            //Constructor(Kurucu) metot
+            //Nesne olusturulunca otomatik olarak calistirilmasi gerekenler burada 
+            //this -> sinifin elemani demek
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+        public Calisan(string ad, string soyad){
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
         public void CalisanBilgileri(){
             Console.WriteLine("Calisan Adi: {0}",Ad);
             Console.WriteLine("Calisan Soyadi: {0}",Soyad);
