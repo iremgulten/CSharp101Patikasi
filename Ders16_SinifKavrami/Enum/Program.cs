12d83ecbba027e32034a19e4246c48ffd83b5a89
İrem Gülten <iremgulten@gmail.com>
2021-06-01 11:05:55 +0300

    Struct yapisi incelendi

---
+++ Ders16_SinifKavrami/Enum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enum -> Ardişik indekslerle calismamizi gerektiren durumlarda kullanilir.
+            Console.WriteLine(Gunler.Pazar); //Pazar
+            Console.WriteLine((int)Gunler.Cumartesi); //6
+            //Cuma = 23 dersek
+            Console.WriteLine((int)Gunler.Cumartesi); //24
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Disariya cikmak icin havanin biraz daha isinmasini bekleyelim.");            
+            }else if(sicaklik >= (int)HavaDurumu.Sicak){
+                Console.WriteLine("Disariya cikmak icin cok sicak bir gun");
+            }else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak){
+                Console.WriteLine("Hadi disariya cikalim!");
+            }
+
+            //Output -> Disariya cikmak icin cok sicak bir gun
+
+        }
+    }
+    enum Gunler{
+        Pazartesi = 1, //0dan degil 1den baslar indeksleme
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu{
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
---
+++ Ders16_SinifKavrami/StructYapisi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace StructYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Struct -> Class kullanmayi gerektirecek kadar komplex olmayan yapilar icin veri kapsullemeyi struct yapar
+            Struct ya da classtan kalitilamazken inheritancedan kalitilabilir
+            */
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar =3;
+            dikdortgen.UzunKenar =4;
+
+            Console.WriteLine("Class alan hesabi: {0}",dikdortgen.AlanHesapla());
+
+            DikdortgenStruct dikdortgenStruct = new DikdortgenStruct(3,4);
+            //Struct olustururken new kullanmadan da direkt olusturup fieldlarina erisebilirim
+
+            Console.WriteLine("Struct alan hesabi: {0}",dikdortgenStruct.AlanHesapla());
+
+        }
+    }
+
+    class Dikdortgen{
+        public int KisaKenar;
+        public int UzunKenar;
+        public Dikdortgen(){
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+        public long AlanHesapla(){
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+    struct DikdortgenStruct{
+        public int KisaKenar;
+        public int UzunKenar;
+        public DikdortgenStruct(int kisaKenar, int uzunKenar)
+        { //Parametresi olmayan constructor tanimlanamaz
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+        public long AlanHesapla(){
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
